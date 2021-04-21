7ba059777e8ca2b43c68c2f66dbfde77b68ad5fb
Raul Rodriguez <raulrobi@gmail.com>
2021-04-20 21:20:37 -0300

    Class 305 - Calendar aditional configurations

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,12 +1,74 @@
 import React from "react";
 import {Navbar} from "../ui/Navbar";
 
+import {Calendar, momentLocalizer} from "react-big-calendar";
+import moment from "moment";
+
+import {messages} from "../../helpers/calendarMessagesEsp";
+
+//React big calendar styles
+import "react-big-calendar/lib/css/react-big-calendar.css";
+//Moment locale Spanish configuration
+import "moment/locale/es";
+import {
+    months,
+    monthsShort,
+    monthsShortDot,
+    weekdays,
+    weekdaysShort
+} from "./configurations";
+
+moment.locale("es");
+
+//Modifica los nombres de meses y días poniendole la primer letra en mayúsculas
+moment.updateLocale("es", {
+    months: months,
+    weekdays: weekdays,
+    monthsShort: monthsShort,
+    monthsShortDot: monthsShortDot,
+    weekdaysShort: weekdaysShort
+});
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+    {
+        title: "Entrevista de trabajo",
+        start: moment().toDate(),
+        end: moment().add(2, "hours").toDate(),
+        bgcolor: "#fafafa"
+    }
+];
+
 export const CalendarScreen = () => {
+    //Crea un estilo para aplicar
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        //console.log(event, start, end, isSelected);
+
+        const style = {
+            backgroundColor: "#367cf7",
+            borderRadius: "0px",
+            opacity: 0.8,
+            display: "block",
+            color: "white"
+        };
+
+        return {style};
+    };
+
     return (
-        <div>
-            <h1>
-                <Navbar />
-            </h1>
+        <div className="calendar-screen">
+            <Navbar />
+
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{height: 500}}
+                messages={messages}
+                eventPropGetter={eventStyleGetter}
+            />
         </div>
     );
 };
---
+++ src/components/calendar/configurations.js
@@ -0,0 +1,16 @@
+export const months = "Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre".split(
+    "_"
+);
+export const monthsShort = "Ene_Feb_Mar_Abr_May_Jun_Jul_Ago_Sep_Oct_Nov_Dic".split(
+    "_"
+);
+
+export const monthsShortDot = "Ene._Feb._Mar._Abr._May._Jun._Jul._Ago._Sep._Oct._Nov._Dic.".split(
+    "_"
+);
+
+export const weekdays = "Domingo_Lunes_Martes_Miércoles_Jueves_Viernes_Sábado".split(
+    "_"
+);
+
+export const weekdaysShort = "Dom._Lun._Mar._Mié._Jue._Vie._Sáb.".split("_");
