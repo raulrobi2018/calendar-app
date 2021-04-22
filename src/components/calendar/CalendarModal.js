899bf1af1a09a81c7e3077013e7fefbca4f3c99e
Raul Rodriguez <raulrobi@gmail.com>
2021-04-22 09:18:28 -0300

    Class 308 - Creating a datepicker form over the modal

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import Modal from "react-modal";
+import DateTimePicker from "react-datetime-picker";
+import moment from "moment";
 
 const customStyles = {
     content: {
@@ -15,18 +17,28 @@ const customStyles = {
 // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
 Modal.setAppElement("#root");
 
-export const CalendarModal = () => {
-    //Utilizamos useState para manejar el estado del modal pero
-    //más adelante será manejado con Redux
-    const [isOpen, setIsOpen] = useState(true);
+//Lo inicializa en la hora actual más 1 hora
+const dateNow = moment().minutes(0).seconds(0).add(1, "hours");
+const dateFuture = dateNow.clone().add(1, "hours");
 
-    const closeModal = () => {
-        setIsOpen(false);
+export const CalendarModal = () => {
+    //Mantiene el estado de la fecha seleccionada en el campo del datepicker
+    const [dateStart, setDateStart] = useState(dateNow.toDate());
+    const [dateEnd, setDateEnd] = useState(dateFuture.toDate());
+
+    const closeModal = () => {};
+
+    const handleStartDateChange = (e) => {
+        setDateStart(e);
+    };
+
+    const handleEndDateChange = (e) => {
+        setDateEnd(e);
     };
 
     return (
         <Modal
-            isOpen={isOpen}
+            isOpen={true}
             // onAfterOpen={afterOpenModal}
             onRequestClose={closeModal}
             style={customStyles}
@@ -34,7 +46,64 @@ export const CalendarModal = () => {
             overlayClassName="modal-fondo"
             closeTimeoutMS={200}
         >
-            <h1>Este es el modal</h1>
+            <h1> Nuevo evento </h1>
+            <hr />
+            <form className="container">
+                <div className="form-group">
+                    <label>Fecha y hora inicio</label>
+                    <DateTimePicker
+                        onChange={handleStartDateChange}
+                        value={dateStart}
+                        className="form-control"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Fecha y hora fin</label>
+                    <DateTimePicker
+                        onChange={handleEndDateChange}
+                        value={dateEnd}
+                        className="form-control"
+                        minDate={dateStart}
+                    />
+                </div>
+
+                <hr />
+                <div className="form-group">
+                    <label>Titulo y notas</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Título del evento"
+                        name="title"
+                        autoComplete="off"
+                    />
+                    <small id="emailHelp" className="form-text text-muted">
+                        Una descripción corta
+                    </small>
+                </div>
+
+                <div className="form-group">
+                    <textarea
+                        type="text"
+                        className="form-control"
+                        placeholder="Notas"
+                        rows="5"
+                        name="notes"
+                    ></textarea>
+                    <small id="emailHelp" className="form-text text-muted">
+                        Información adicional
+                    </small>
+                </div>
+
+                <button
+                    type="submit"
+                    className="btn btn-outline-primary btn-block"
+                >
+                    <i className="far fa-save"></i>
+                    <span> Guardar</span>
+                </button>
+            </form>
         </Modal>
     );
 };
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import {CalendarApp} from "./CalendarApp";
 
 import "./styles.css";
 import "./components/calendar/modal.css";
+import "./components/calendar/datepicker.css";
 
 ReactDOM.render(<CalendarApp />, document.getElementById("root"));
