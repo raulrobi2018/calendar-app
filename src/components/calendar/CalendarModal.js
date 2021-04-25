a155b3eb2a3f946462cc7480c8a785cb56bc17fb
Raul Rodriguez <raulrobi@gmail.com>
2021-04-25 09:45:19 -0300

    Class 314 - Add a new event

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,21 +1,22 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Modal from "react-modal";
 import DateTimePicker from "react-datetime-picker";
 import moment from "moment";
 
 import Swal from "sweetalert2";
-import { useDispatch, useSelector } from "react-redux";
-import { uiCloseModal } from "../../actions/ui";
+import {useDispatch, useSelector} from "react-redux";
+import {uiCloseModal} from "../../actions/ui";
+import {eventAddNew} from "../../actions/events";
 
 const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)"
-  }
+    content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)"
+    }
 };
 
 // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
@@ -26,149 +27,165 @@ const dateNow = moment().minutes(0).seconds(0).add(1, "hours");
 const dateFuture = dateNow.clone().add(1, "hours");
 
 export const CalendarModal = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  //Hook de Redux que retorna el state actual
-  //En este caso tomo el atributo ui del state y luego desestructuro el modalOpen
-  const { modalOpen } = useSelector((state) => {
-    return state.ui;
-  });
-
-  //Mantiene el estado de la fecha seleccionada en el campo del datepicker
-  const [dateStart, setDateStart] = useState(dateNow.toDate());
-  const [dateEnd, setDateEnd] = useState(dateFuture.toDate());
-  const [validTitle, setValidTitle] = useState(true);
-
-  //Mantiene el estado del formulario para agregar un nuevo evento
-  const [formValues, setFormValues] = useState({
-    title: "",
-    notes: "",
-    start: dateNow.toDate(),
-    end: dateFuture.toDate()
-  });
-
-  const { title, notes, start, end } = formValues;
-
-  const handleInputChange = ({ target }) => {
-    setFormValues({
-      //primero establezco todos los valores del formulario
-      ...formValues,
-      //para el campo que estoy cambiando, toma su nombre y asigna el value
-      [target.name]: target.value
+    //Hook de Redux que retorna el state actual
+    //En este caso tomo el atributo ui del state y luego desestructuro el modalOpen
+    const {modalOpen} = useSelector((state) => {
+        return state.ui;
     });
-  };
 
-  const closeModal = () => {
-    dispatch(uiCloseModal());
-  };
+    //Mantiene el estado de la fecha seleccionada en el campo del datepicker
+    const [dateStart, setDateStart] = useState(dateNow.toDate());
+    const [dateEnd, setDateEnd] = useState(dateFuture.toDate());
+    const [validTitle, setValidTitle] = useState(true);
 
-  const handleStartDateChange = (e) => {
-    setDateStart(e);
-    setFormValues({
-      ...formValues,
-      start: e
+    //Mantiene el estado del formulario para agregar un nuevo evento
+    const [formValues, setFormValues] = useState({
+        title: "",
+        notes: "",
+        start: dateNow.toDate(),
+        end: dateFuture.toDate()
     });
-  };
 
-  const handleEndDateChange = (e) => {
-    setDateEnd(e);
-    setFormValues({
-      ...formValues,
-      end: e
-    });
-  };
+    const {title, notes, start, end} = formValues;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const handleInputChange = ({target}) => {
+        setFormValues({
+            //primero establezco todos los valores del formulario
+            ...formValues,
+            //para el campo que estoy cambiando, toma su nombre y asigna el value
+            [target.name]: target.value
+        });
+    };
 
-    //Creo instancias de moment
-    const momentStart = moment(start);
-    const momentEnd = moment(end);
-    if (momentStart.isSameOrAfter(momentEnd)) {
-      Swal.fire("Error", "La fecha de fin debe ser mayor a la de inicio");
-      return;
-    }
+    const closeModal = () => {
+        dispatch(uiCloseModal());
+    };
 
-    if (title.trim().length < 10) {
-      return setValidTitle(false);
-    }
+    const handleStartDateChange = (e) => {
+        setDateStart(e);
+        setFormValues({
+            ...formValues,
+            start: e
+        });
+    };
 
-    //TODO: grabar en base de datos
+    const handleEndDateChange = (e) => {
+        setDateEnd(e);
+        setFormValues({
+            ...formValues,
+            end: e
+        });
+    };
 
-    setValidTitle(true);
-    closeModal();
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-  return (
-    <Modal
-      isOpen={modalOpen}
-      // onAfterOpen={afterOpenModal}
-      onRequestClose={closeModal}
-      style={customStyles}
-      className="modal"
-      overlayClassName="modal-fondo"
-      closeTimeoutMS={200}
-    >
-      <h1> Nuevo evento </h1>
-      <hr />
-      <form className="container" onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Fecha y hora inicio</label>
-          <DateTimePicker
-            onChange={handleStartDateChange}
-            value={dateStart}
-            className="form-control"
-          />
-        </div>
+        //Creo instancias de moment
+        const momentStart = moment(start);
+        const momentEnd = moment(end);
+        if (momentStart.isSameOrAfter(momentEnd)) {
+            Swal.fire("Error", "La fecha de fin debe ser mayor a la de inicio");
+            return;
+        }
 
-        <div className="form-group">
-          <label>Fecha y hora fin</label>
-          <DateTimePicker
-            onChange={handleEndDateChange}
-            value={dateEnd}
-            className="form-control"
-            minDate={dateStart}
-          />
-        </div>
+        if (title.trim().length < 10) {
+            return setValidTitle(false);
+        }
 
-        <hr />
-        <div className="form-group">
-          <label>Titulo y notas</label>
-          <input
-            type="text"
-            className={`form-control ${!validTitle && "is-invalid"}`}
-            placeholder="Título del evento"
-            name="title"
-            autoComplete="off"
-            value={title}
-            onChange={handleInputChange}
-            required="required"
-          />
-          <small id="emailHelp" className="form-text text-muted">
-            Una descripción corta
-          </small>
-        </div>
+        //A la acción le paso el evento y le agrego un id y el usuario
+        dispatch(
+            eventAddNew({
+                ...formValues,
+                id: new Date().getTime(),
+                user: {
+                    _id: 1321312,
+                    name: "Raul Rodriguez"
+                }
+            })
+        );
+        //TODO: grabar en base de datos
 
-        <div className="form-group">
-          <textarea
-            type="text"
-            className="form-control"
-            placeholder="Notas"
-            rows="5"
-            name="notes"
-            value={notes}
-            onChange={handleInputChange}
-          ></textarea>
-          <small id="emailHelp" className="form-text text-muted">
-            Información adicional
-          </small>
-        </div>
+        setValidTitle(true);
+        closeModal();
+    };
 
-        <button type="submit" className="btn btn-outline-primary btn-block">
-          <i className="far fa-save"></i>
-          <span> Guardar</span>
-        </button>
-      </form>
-    </Modal>
-  );
+    return (
+        <Modal
+            isOpen={modalOpen}
+            // onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            style={customStyles}
+            className="modal"
+            overlayClassName="modal-fondo"
+            closeTimeoutMS={200}
+        >
+            <h1> Nuevo evento </h1>
+            <hr />
+            <form className="container" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Fecha y hora inicio</label>
+                    <DateTimePicker
+                        onChange={handleStartDateChange}
+                        value={dateStart}
+                        className="form-control"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Fecha y hora fin</label>
+                    <DateTimePicker
+                        onChange={handleEndDateChange}
+                        value={dateEnd}
+                        className="form-control"
+                        minDate={dateStart}
+                    />
+                </div>
+
+                <hr />
+                <div className="form-group">
+                    <label>Titulo y notas</label>
+                    <input
+                        type="text"
+                        className={`form-control ${
+                            !validTitle && "is-invalid"
+                        }`}
+                        placeholder="Título del evento"
+                        name="title"
+                        autoComplete="off"
+                        value={title}
+                        onChange={handleInputChange}
+                        required="required"
+                    />
+                    <small id="emailHelp" className="form-text text-muted">
+                        Una descripción corta
+                    </small>
+                </div>
+
+                <div className="form-group">
+                    <textarea
+                        type="text"
+                        className="form-control"
+                        placeholder="Notas"
+                        rows="5"
+                        name="notes"
+                        value={notes}
+                        onChange={handleInputChange}
+                    ></textarea>
+                    <small id="emailHelp" className="form-text text-muted">
+                        Información adicional
+                    </small>
+                </div>
+
+                <button
+                    type="submit"
+                    className="btn btn-outline-primary btn-block"
+                >
+                    <i className="far fa-save"></i>
+                    <span> Guardar</span>
+                </button>
+            </form>
+        </Modal>
+    );
 };
--- src/reducers/calendarReducer.js
+++ src/reducers/calendarReducer.js
@@ -1,20 +1,22 @@
-import moment from 'moment';
-import { types } from '../types/types';
+import moment from "moment";
+import {types} from "../types/types";
 
 const initialState = {
-    events: [{
-        title: "Entrevista de trabajo",
-        start: moment().toDate(),
-        end: moment().add(2, "hours").toDate(),
-        bgcolor: "#fafafa",
-        notes: "Practicar preguntas clave",
-        user: {
-            _id: 123,
-            name: "Raul Rodriguez"
+    events: [
+        {
+            title: "Entrevista de trabajo",
+            start: moment().toDate(),
+            end: moment().add(2, "hours").toDate(),
+            bgcolor: "#fafafa",
+            notes: "Practicar preguntas clave",
+            user: {
+                _id: 123,
+                name: "Raul Rodriguez"
+            }
         }
-    }],
+    ],
     activeEvent: null
-}
+};
 
 export const calendarReducer = (state = initialState, action) => {
     switch (action.type) {
@@ -22,10 +24,16 @@ export const calendarReducer = (state = initialState, action) => {
             return {
                 ...state,
                 activeEvent: action.payload
-            }
+            };
             break;
+        case types.eventAddNew:
+            return {
+                ...state,
+                //Expando los eventos y le agrego el nuevo
+                events: [...state.events, action.payload]
+            };
         default:
             return state;
             break;
     }
-}
+};
