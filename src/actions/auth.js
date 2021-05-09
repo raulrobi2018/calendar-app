6126b4be294c19e6f1f783d70c71531042e0f216
Raul Rodriguez <raulrobi@gmail.com>
2021-05-08 21:33:28 -0300

    Cambios

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,94 +1,94 @@
-import {fetchWithOutToken, fetchWithToken} from "../helpers/fetch";
-import {types} from "../types/types";
+import { fetchWithOutToken, fetchWithToken } from "../helpers/fetch";
+import { types } from "../types/types";
 import Swal from "sweetalert2";
-import {clearEventsOnLogout} from "./events";
+import { clearEventsOnLogout } from "./events";
 
 export const startLogin = (email, password) => {
-    return async (dispatch) => {
-        const resp = await fetchWithOutToken("auth", {email, password}, "POST");
-        const body = await resp.json();
+  return async (dispatch) => {
+    const resp = await fetchWithOutToken("auth", { email, password }, "POST");
+    const body = await resp.json();
 
-        if (body.ok) {
-            localStorage.setItem("token", body.token);
-            //Se configuró en 2 horas
-            localStorage.setItem("token-init-date", new Date().getTime());
+    if (body.ok) {
+      localStorage.setItem("token", body.token);
+      //Se configuró en 2 horas
+      localStorage.setItem("token-init-date", new Date().getTime());
 
-            dispatch(
-                login({
-                    uid: body.uid,
-                    name: body.name
-                })
-            );
-        } else {
-            Swal.fire("Error", body.msg, "error");
-        }
-    };
+      dispatch(
+        login({
+          uid: body.uid,
+          name: body.name
+        })
+      );
+    } else {
+      Swal.fire("Error", body.msg, "error");
+    }
+  };
 };
 
 const login = (user) => ({
-    type: types.authLogin,
-    payload: user
+  type: types.authLogin,
+  payload: user
 });
 
 export const startRegister = (name, email, password) => {
-    return async (dispatch) => {
-        const resp = await fetchWithOutToken(
-            "auth/new",
-            {name, email, password},
-            "POST"
-        );
+  return async (dispatch) => {
+    const resp = await fetchWithOutToken(
+      "auth/new",
+      { name, email, password },
+      "POST"
+    );
 
-        const body = await resp.json();
+    const body = await resp.json();
 
-        if (body.ok) {
-            localStorage.setItem("token", body.token);
-            //Se configuró en 2 horas
-            localStorage.setItem("token-init-date", new Date().getTime());
+    if (body.ok) {
+      localStorage.setItem("token", body.token);
+      //Se configuró en 2 horas
+      localStorage.setItem("token-init-date", new Date().getTime());
 
-            dispatch(
-                login({
-                    uid: body.uid,
-                    name: body.name
-                })
-            );
-        } else {
-            Swal.fire("Error", body.msg, "error");
-        }
-    };
+      dispatch(
+        login({
+          uid: body.uid,
+          name: body.name
+        })
+      );
+    } else {
+      Swal.fire("Error", body.msg, "error");
+    }
+  };
 };
 
 export const startChecking = () => {
-    return async (dispatch) => {
-        const resp = await fetchWithToken("auth/renew");
+  return async (dispatch) => {
+    const resp = await fetchWithToken("auth/renew");
 
-        const body = await resp.json();
+    const body = await resp.json();
 
-        if (body.ok) {
-            localStorage.setItem("token", body.token);
-            //Se configuró en 2 horas
-            localStorage.setItem("token-init-date", new Date().getTime());
+    if (body.ok) {
+      localStorage.setItem("token", body.token);
+      //Se configuró en 2 horas
+      localStorage.setItem("token-init-date", new Date().getTime());
 
-            dispatch(
-                login({
-                    uid: body.uid,
-                    name: body.name
-                })
-            );
-        } else {
-            dispatch(checkingFinish());
-        }
-    };
+      dispatch(
+        login({
+          uid: body.uid,
+          name: body.name
+        })
+      );
+    } else {
+      dispatch(checkingFinish());
+    }
+  };
 };
 
-const checkingFinish = () => ({type: types.authCheckingFinish});
+const checkingFinish = () => ({ type: types.authCheckingFinish });
 
 export const startLogout = () => {
-    return (dispatch) => {
-        //
-        localStorage.clear();
-        dispatch(logout());
-        dispatch(clearEventsOnLogout());
-    };
+  return (dispatch) => {
+    //
+    localStorage.clear();
+    dispatch(logout());
+    dispatch(clearEventsOnLogout());
+  };
 };
 
-const logout = () => ({type: types.authLogout});
+const logout = () => ({ type: types.authLogout });
--- src/components/calendar/CalendarEvent.js
+++ src/components/calendar/CalendarEvent.js
@@ -1,14 +1,12 @@
 import React from "react";
 
 //event es el evento en el calendario
-export const CalendarEvent = ({event}) => {
-    const {title, user} = event;
+export const CalendarEvent = ({ event }) => {
+  const { title, user } = event;
 
-    return (
-        <div>
-            <strong>{title}</strong>
-            <br />
-            <span>{user.name}</span>
-        </div>
-    );
+  return (
+    <div>
+      <strong>{title}</strong>
+    </div>
+  );
 };
